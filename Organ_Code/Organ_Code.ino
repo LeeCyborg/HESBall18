df6069db4ecad89cd04edb5f6bf644be0f1fbbf4
Linbot <lindywilkins@gmail.com>
2018-08-26 14:56:55 -0400

    Initial commit

---
+++ Organ_Code/Organ_Code.ino
@@ -0,0 +1,18 @@
+void setup() {
+  // put your setup code here, to run once:
+  for (int i = 2; i < 18; i++) {
+    pinMode(i, INPUT_PULLUP);
+  }
+int codes[][6] =   { {1,2,3,4,5,6},
+                    {6,5,4,3,2,1} };
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
+
+int boopit(){ 
+return 12;
+}
+
