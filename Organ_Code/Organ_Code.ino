ff8351ce7fb9b12486c38c2e8a0897546ae4d8ad
Linbot <lindywilkins@gmail.com>
2018-08-26 15:05:12 -0400

    Code assignments

--- Organ_Code/Organ_Code.ino
+++ Organ_Code/Organ_Code.ino
@@ -1,18 +1,32 @@
 void setup() {
-  // put your setup code here, to run once:
+  /* Pin assignments
+   *  ===Colors===
+   *  Red 2
+   *  yellow 3
+   *  green 4
+   *  blue 5
+   *  ===Additions===
+   *  25 points 6
+   *  10 points 7
+   *  5 points 8
+   *  1 point 9
+   *  - points 10
+   *  ============
+   *  22-27 code buttons
+   */
+  int pinAssignments[18];
   for (int i = 2; i < 18; i++) {
     pinMode(i, INPUT_PULLUP);
   }
+
 int codes[][6] =   { {1,2,3,4,5,6},
-                    {6,5,4,3,2,1} };
+                     {6,5,4,3,2,1} };
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
 
 }
-
-int boopit(){ 
-return 12;
+bool checkIt(int code[]){
+  return true;
 }
 
